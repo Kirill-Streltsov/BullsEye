4c27605816af51f1489d05a397a4e40efa806943
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-09-02 11:53:43 +0200

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Kirill Streltsov on 02.09.23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
